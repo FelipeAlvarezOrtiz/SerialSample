0461239fab06572dae684f91b2f17e210381f6e3
Felipe Alvarez Ortiz <falvarezortiz@hotmail.com>
2018-10-18 10:31:26 -0300

    Sample

--- SerialSample/Form1.Designer.cs
+++ SerialSample/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SerialSample
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -29,12 +29,220 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.inputLabel = new System.Windows.Forms.Label();
+            this.cbSerials = new System.Windows.Forms.ComboBox();
+            this.refreshButton = new System.Windows.Forms.Button();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.baudLabel = new System.Windows.Forms.Label();
+            this.baudCombo = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textRecibidos = new System.Windows.Forms.RichTextBox();
+            this.puertoSerial = new System.IO.Ports.SerialPort(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.parityBox = new System.Windows.Forms.ComboBox();
+            this.dataBitsBox = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.dissConnect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // inputLabel
+            // 
+            this.inputLabel.Location = new System.Drawing.Point(35, 38);
+            this.inputLabel.Name = "inputLabel";
+            this.inputLabel.Size = new System.Drawing.Size(124, 23);
+            this.inputLabel.TabIndex = 0;
+            this.inputLabel.Text = "Puertos Seriales: ";
+            this.inputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // cbSerials
+            // 
+            this.cbSerials.FormattingEnabled = true;
+            this.cbSerials.Location = new System.Drawing.Point(165, 38);
+            this.cbSerials.Name = "cbSerials";
+            this.cbSerials.Size = new System.Drawing.Size(213, 24);
+            this.cbSerials.TabIndex = 1;
+            // 
+            // refreshButton
+            // 
+            this.refreshButton.Location = new System.Drawing.Point(408, 31);
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(97, 37);
+            this.refreshButton.TabIndex = 2;
+            this.refreshButton.Text = "Resfrescar";
+            this.refreshButton.UseVisualStyleBackColor = true;
+            this.refreshButton.Click += new System.EventHandler(this.resfreshClick);
+            // 
+            // connectButton
+            // 
+            this.connectButton.Location = new System.Drawing.Point(580, 28);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(148, 42);
+            this.connectButton.TabIndex = 3;
+            this.connectButton.Text = "Conectar";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.ConnectClick);
+            // 
+            // baudLabel
+            // 
+            this.baudLabel.Location = new System.Drawing.Point(35, 86);
+            this.baudLabel.Name = "baudLabel";
+            this.baudLabel.Size = new System.Drawing.Size(124, 23);
+            this.baudLabel.TabIndex = 4;
+            this.baudLabel.Text = "Baud Rate:";
+            this.baudLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // baudCombo
+            // 
+            this.baudCombo.FormattingEnabled = true;
+            this.baudCombo.Items.AddRange(new object[] {
+            "1200",
+            "2400",
+            "4800",
+            "9600",
+            "14400",
+            "19200",
+            "28800",
+            "38400",
+            "56000",
+            "57600",
+            "115200",
+            "128000",
+            "256000"});
+            this.baudCombo.Location = new System.Drawing.Point(165, 86);
+            this.baudCombo.Name = "baudCombo";
+            this.baudCombo.Size = new System.Drawing.Size(213, 24);
+            this.baudCombo.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(35, 251);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 23);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Datos Recibidos:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textRecibidos
+            // 
+            this.textRecibidos.Location = new System.Drawing.Point(180, 250);
+            this.textRecibidos.Name = "textRecibidos";
+            this.textRecibidos.Size = new System.Drawing.Size(409, 85);
+            this.textRecibidos.TabIndex = 7;
+            this.textRecibidos.Text = "";
+            // 
+            // puertoSerial
+            // 
+            this.puertoSerial.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.dataRecibida);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(35, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 23);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "DataBits:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(35, 176);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 23);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Parity:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // parityBox
+            // 
+            this.parityBox.FormattingEnabled = true;
+            this.parityBox.Items.AddRange(new object[] {
+            "None",
+            "Even",
+            "Odd",
+            "Mark",
+            "Space"});
+            this.parityBox.Location = new System.Drawing.Point(165, 176);
+            this.parityBox.Name = "parityBox";
+            this.parityBox.Size = new System.Drawing.Size(212, 24);
+            this.parityBox.TabIndex = 10;
+            // 
+            // dataBitsBox
+            // 
+            this.dataBitsBox.FormattingEnabled = true;
+            this.dataBitsBox.Items.AddRange(new object[] {
+            "5",
+            "6",
+            "7",
+            "8"});
+            this.dataBitsBox.Location = new System.Drawing.Point(166, 134);
+            this.dataBitsBox.Name = "dataBitsBox";
+            this.dataBitsBox.Size = new System.Drawing.Size(212, 24);
+            this.dataBitsBox.TabIndex = 11;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(408, 89);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(106, 21);
+            this.checkBox1.TabIndex = 13;
+            this.checkBox1.Text = "Discard Null";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // dissConnect
+            // 
+            this.dissConnect.Location = new System.Drawing.Point(580, 89);
+            this.dissConnect.Name = "dissConnect";
+            this.dissConnect.Size = new System.Drawing.Size(148, 42);
+            this.dissConnect.TabIndex = 14;
+            this.dissConnect.Text = "Desconectar";
+            this.dissConnect.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(780, 358);
+            this.Controls.Add(this.dissConnect);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.dataBitsBox);
+            this.Controls.Add(this.parityBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textRecibidos);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.baudCombo);
+            this.Controls.Add(this.baudLabel);
+            this.Controls.Add(this.connectButton);
+            this.Controls.Add(this.refreshButton);
+            this.Controls.Add(this.cbSerials);
+            this.Controls.Add(this.inputLabel);
+            this.Name = "MainForm";
+            this.Text = "Configuracion de Serial Port";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label inputLabel;
+        private System.Windows.Forms.ComboBox cbSerials;
+        private System.Windows.Forms.Button refreshButton;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.Label baudLabel;
+        private System.Windows.Forms.ComboBox baudCombo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox textRecibidos;
+        private System.IO.Ports.SerialPort puertoSerial;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox parityBox;
+        private System.Windows.Forms.ComboBox dataBitsBox;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button dissConnect;
     }
 }
 
--- SerialSample/Form1.cs
+++ SerialSample/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,126 @@ using System.Windows.Forms;
 
 namespace SerialSample
 {
-    public partial class Form1 : Form
+    //+569 44194679 me hablas al whathsapp si tienes alguna duda
+    public partial class MainForm : Form
     {
-        public Form1()
+        private string strBufferIn;
+        private string strBufferOut;
+
+        public MainForm()
         {
             InitializeComponent();
+        }
+
+        //Aqui Busca los puertos libres y los agrega al combobox
+        private void resfreshClick(object sender, EventArgs e)
+        {
+            //Deshabilitamos el boton hasta detectar puertos compatibles
+            connectButton.Enabled = false;
+            //Limpiamos ComboBox y buscamos todos los nombres de los puertos disponibles
+            cbSerials.Items.Clear();
+            foreach (string puertoSingle in SerialPort.GetPortNames())
+            {
+                cbSerials.Items.Add(puertoSingle);
+
+            }
+            if (cbSerials.Items.Count > 0)
+            {
+                cbSerials.SelectedIndex = 0;
+                connectButton.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show(this, "No hay puertos detectados", "Error");
+                return;
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            //Limpiamos los buffers
+            strBufferIn = string.Empty;
+            strBufferOut = string.Empty;
+            connectButton.Enabled = false;
+
+        }
+
+        private void ConnectClick(object sender, EventArgs e)
+        {
+            string paridad = parityBox.Text;
+            bool discardNull = checkBox1.Checked;
+
+            try
+            {
+                //Puerto serial es nuestra variable SerialPort
+                //Seteamos los baudrates haciendo un parse desde el combobox
+                puertoSerial.BaudRate = Int32.Parse(baudCombo.Text);
+                puertoSerial.PortName = cbSerials.Text;
+                //Seteamos si se descartaran los Null
+                puertoSerial.DiscardNull = discardNull ? true : false;
+                //Aqui hay varias llaves según la paridad
+                #region Paridad
+                if (paridad.CompareTo("None") == 0)
+                {
+                    puertoSerial.Parity = Parity.None;
+                }
+                else if (paridad.CompareTo("Even") == 0)
+                {
+                    puertoSerial.Parity = Parity.Even;
+                }
+                else if (paridad.CompareTo("Mark") == 0)
+                {
+                    puertoSerial.Parity = Parity.Mark;
+                }
+                else if (paridad.CompareTo("Odd") == 0)
+                {
+                    puertoSerial.Parity = Parity.Odd;
+
+                }
+                else if (paridad.CompareTo("Space") == 0)
+                {
+                    puertoSerial.Parity = Parity.Space;
+                }
+                #endregion
+
+                //seteamos los dataBits
+                puertoSerial.DataBits = Int32.Parse(dataBitsBox.Text);
+                //Seteamos los stopbits
+                puertoSerial.StopBits = StopBits.One;
+                //Seteamos si hay HandShake
+                puertoSerial.Handshake = Handshake.None;
+                //Abrimos el puerto
+                try
+                {
+                    puertoSerial.Open();
+
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show(this,"Error al abrir la comunación con el puerto","Error");
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(this, "Error: " + error.Message, "Error");
+            }
+        }
+
+        //Evento de data recibida, aquí si lo aplicas inmediatamente saltará un error
+        //ya que intenta ingresar al form desde el puerto serial lo cual no esta permitido en .NET
+        //La solución es crear un delegado y como parametro el ReadExisting, que es lo que venga desde
+        //el puerto serial
+        private void dataRecibida(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                textRecibidos.Text = string.Empty;
+                textRecibidos.Text = puertoSerial.ReadExisting();
+
+            }catch(InvalidOperationException messageError)
+            {
+                MessageBox.Show(this,"Error: "+messageError.Message,"Error");
+            }
         }
     }
 }
